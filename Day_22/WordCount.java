9a9145a5ac3368f7383c2aa3acc2aa4718584e2f
madhav404 <dixitmadhav2006@gmail.com>
2026-06-22 21:59:17 +0530

    added day 22 java programe

---
+++ Day_22/WordCount.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class WordCount {
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+
+        int count = 1;
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str.charAt(i) == ' ')
+            {
+                count++;
+            }
+        }
+
+        System.out.println("Number of words = " + count);
+    }
+}
